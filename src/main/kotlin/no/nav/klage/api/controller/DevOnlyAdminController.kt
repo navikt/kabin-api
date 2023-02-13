8133f92e588a62b7970ff7602d277744fff25526
oyvindwedoe <oyvind.norsted.wedoe@nav.no>
2023-02-13 10:46:16 +0100

    Get token in dev env.

---
+++ src/main/kotlin/no/nav/klage/api/controller/DevOnlyAdminController.kt
@@ -0,0 +1,19 @@
+package no.nav.klage.api.controller
+
+import no.nav.klage.util.TokenUtil
+import no.nav.security.token.support.core.api.Unprotected
+import org.springframework.context.annotation.Profile
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@Profile("dev-gcp")
+@RestController
+class DevOnlyAdminController(
+    private val tokenUtil: TokenUtil,
+) {
+    @Unprotected
+    @GetMapping("/internal/mytoken")
+    fun getToken(): String {
+        return tokenUtil.getAccessTokenFrontendSent()
+    }
+}
