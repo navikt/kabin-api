d03a84af575fc9b1187b70aa9b31f238d6c8cbc6
Andreas Jonsson <andreas.jonsson@nav.no>
2023-02-15 08:47:49 +0100

    Added validation "mottatt dato"

--- src/main/kotlin/no/nav/klage/service/KabalApiService.kt
+++ src/main/kotlin/no/nav/klage/service/KabalApiService.kt
@@ -4,7 +4,11 @@ import no.nav.klage.api.controller.view.CreateAnkeBasedOnKlagebehandling
 import no.nav.klage.api.controller.view.IdnummerInput
 import no.nav.klage.api.controller.view.SearchPartInput
 import no.nav.klage.clients.KabalApiClient
+import no.nav.klage.exceptions.InvalidProperty
+import no.nav.klage.exceptions.SectionedValidationErrorWithDetailsException
+import no.nav.klage.exceptions.ValidationSection
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 
 @Service
 class KabalApiService(
@@ -16,10 +20,38 @@ class KabalApiService(
     }
 
     fun createAnkeInKabal(input: CreateAnkeBasedOnKlagebehandling) {
+        validate(input)
         kabalApiClient.createAnkeInKabal(input)
     }
 
     fun searchPart(searchPartInput: SearchPartInput): KabalApiClient.PartView {
         return kabalApiClient.searchPart(searchPartInput = searchPartInput)
+    }
+
+    private fun validate(input: CreateAnkeBasedOnKlagebehandling) {
+        val validationErrors = mutableListOf<InvalidProperty>()
+
+        if (input.mottattNav.isAfter(LocalDate.now())) {
+            validationErrors += InvalidProperty(
+                field = CreateAnkeBasedOnKlagebehandling::mottattNav.name,
+                reason = "Dato kan ikke være i fremtiden"
+            )
+        }
+
+        val sectionList = mutableListOf<ValidationSection>()
+
+        if (validationErrors.isNotEmpty()) {
+            sectionList.add(
+                ValidationSection(
+                    section = "saksdata",
+                    properties = validationErrors
+                )
+            )
+
+            throw SectionedValidationErrorWithDetailsException(
+                title = "Validation error",
+                sections = sectionList
+            )
+        }
     }
 }
