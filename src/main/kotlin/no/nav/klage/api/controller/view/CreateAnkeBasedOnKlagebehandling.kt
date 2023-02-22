7e884069d404021335a1e70e223b24c53f1a5742
Andreas Jonsson <andreas.jonsson@nav.no>
2023-02-22 15:07:05 +0100

    Added fristInWeeks

--- src/main/kotlin/no/nav/klage/api/controller/view/CreateAnkeBasedOnKlagebehandling.kt
+++ src/main/kotlin/no/nav/klage/api/controller/view/CreateAnkeBasedOnKlagebehandling.kt
@@ -6,6 +6,7 @@ import java.util.*
 data class CreateAnkeBasedOnKlagebehandling(
     val klagebehandlingId: UUID,
     val mottattNav: LocalDate,
+    val fristInWeeks: Int,
     val klager: OversendtPartId?,
     val fullmektig: OversendtPartId?,
     val ankeDocumentJournalpostId: String,
