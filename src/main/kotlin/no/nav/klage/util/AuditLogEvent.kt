7b2ce0d5581ab9e60b7454609efa26ec3ce8d6b9
Andreas Jonsson <andreas.jonsson@nav.no>
2023-06-19 15:16:28 +0200

    Added audit logging.
    Co-authored-by: Øyvind Wedøe <oyvind.norsted.wedoe@nav.no>

---
+++ src/main/kotlin/no/nav/klage/util/AuditLogEvent.kt
@@ -0,0 +1,22 @@
+package no.nav.klage.util
+
+data class AuditLogEvent(
+    val navIdent: String,
+    val action: Action = Action.ACCESS,
+    val decision: Decision = Decision.PERMIT,
+    val personFnr: String?,
+    val logLevel: Level = Level.INFO,
+    val message: String?,
+) {
+    enum class Level {
+        INFO, WARN
+    }
+
+    enum class Action(val value: String) {
+        ACCESS("audit:access")
+    }
+
+    enum class Decision {
+        PERMIT, DENY
+    }
+}
