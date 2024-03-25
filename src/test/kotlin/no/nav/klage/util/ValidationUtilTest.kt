63f9df9bbb3518cedc631563bfe512e5ece50e07
Andreas Jonsson <andreas.jonsson@nav.no>
2024-03-25 08:57:51 +0100

    Fixed compiling.

--- src/test/kotlin/no/nav/klage/util/ValidationUtilTest.kt
+++ src/test/kotlin/no/nav/klage/util/ValidationUtilTest.kt
@@ -22,7 +22,8 @@ class ValidationUtilTest {
             ytelseId = null,
             hjemmelIdList = null,
             avsender = null,
-            saksbehandlerIdent = null
+            saksbehandlerIdent = null,
+            svarbrevInput = null,
         )
 
         assertThrows<InvalidSourceException> { validationUtil.validateCreateAnkeInputView(input = input) }
