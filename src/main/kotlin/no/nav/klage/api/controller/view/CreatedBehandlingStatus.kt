63638f61d6402e5812cb0a0e33b03aa35fbc61cd
Andreas Jonsson <andreas.jonsson@nav.no>
2024-03-28 11:45:51 +0100

    Svarbrev response in newly created anke.

--- src/main/kotlin/no/nav/klage/api/controller/view/CreatedBehandlingStatus.kt
+++ src/main/kotlin/no/nav/klage/api/controller/view/CreatedBehandlingStatus.kt
@@ -1,6 +1,7 @@
 package no.nav.klage.api.controller.view
 
 import java.time.LocalDate
+import java.util.*
 
 data class CreatedAnkebehandlingStatusView(
     val typeId: String,
@@ -15,7 +16,31 @@ data class CreatedAnkebehandlingStatusView(
     val fagsystemId: String,
     val journalpost: DokumentReferanse,
     val tildeltSaksbehandler: TildeltSaksbehandler?,
-)
+    val svarbrev: Svarbrev?,
+) {
+    data class Svarbrev(
+        val dokumentUnderArbeidId: UUID,
+        val title: String,
+        val receivers: List<Receiver>,
+    ) {
+        data class Receiver(
+            val id: String,
+            val name: String,
+            val address: Address?,
+            val localPrint: Boolean,
+            val forceCentralPrint: Boolean,
+        ) {
+            data class Address(
+                val adresselinje1: String?,
+                val adresselinje2: String?,
+                val adresselinje3: String?,
+                val landkode: String,
+                val postnummer: String?,
+                val poststed: String?,
+            )
+        }
+    }
+}
 
 data class CreatedKlagebehandlingStatusView(
     val typeId: String,
