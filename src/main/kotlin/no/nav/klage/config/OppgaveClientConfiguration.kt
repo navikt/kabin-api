035a26d8f8b1f517b969f6b2627a6d5063ed1288
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-06-05 13:51:18 +0200

    Set header.

--- src/main/kotlin/no/nav/klage/config/OppgaveClientConfiguration.kt
+++ src/main/kotlin/no/nav/klage/config/OppgaveClientConfiguration.kt
@@ -3,6 +3,8 @@ package no.nav.klage.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.netty.http.client.HttpClient.newConnection
@@ -16,6 +18,7 @@ class OppgaveClientConfiguration(private val webClientBuilder: WebClient.Builder
     fun oppgaveWebClient(): WebClient {
         return webClientBuilder
             .baseUrl(oppgaveBaseURL)
+            .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
             .clientConnector(ReactorClientHttpConnector(newConnection()))
             .build()
     }
