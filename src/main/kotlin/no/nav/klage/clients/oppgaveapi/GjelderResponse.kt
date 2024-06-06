258e048d4d9272fc0245ca55b6ed4ddc936cfa38
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-06-06 12:52:13 +0200

    Unused class.

--- src/main/kotlin/no/nav/klage/clients/oppgaveapi/GjelderResponse.kt
+++ src/main/kotlin/no/nav/klage/clients/oppgaveapi/GjelderResponse.kt
@@ -1,9 +1,5 @@
 package no.nav.klage.clients.oppgaveapi
 
-data class GjelderResponse(
-    val gjelder: List<Gjelder>,
-)
-
 data class Gjelder(
     val behandlingsTema: String?,
     val behandlingstemaTerm : String?,
