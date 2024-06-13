b9220885bec406fca1b250399b13d010feec7393
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2024-06-13 08:18:30 +0200

    Reintroduced clientConnector.

--- src/main/kotlin/no/nav/klage/config/OppgaveClientConfiguration.kt
+++ src/main/kotlin/no/nav/klage/config/OppgaveClientConfiguration.kt
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient.newConnection
 
 @Configuration
 class OppgaveClientConfiguration(private val webClientBuilder: WebClient.Builder) {
@@ -18,6 +20,7 @@ class OppgaveClientConfiguration(private val webClientBuilder: WebClient.Builder
             .baseUrl(oppgaveBaseURL)
             .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
             .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .clientConnector(ReactorClientHttpConnector(newConnection()))
             .build()
     }
 
