6f546864e434ae71cbcb2d61542d54f9f27f1837
Andreas Jonsson <andreas.jonsson@nav.no>
2025-01-08 10:28:07 +0100

    Testing downgrading Spring.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val otelVersion = "1.45.0"
 
 plugins {
     val kotlinVersion = "2.1.0"
-    id("org.springframework.boot") version "3.4.1"
+    id("org.springframework.boot") version "3.3.5"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
