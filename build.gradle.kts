bfebbb263f309e242fd23389bf8997f2e1f20101
Andreas Jonsson <andreas.jonsson@nav.no>
2025-01-08 10:46:28 +0100

    Testing downgrading to previous versions (before dependabot).

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val tokenValidationVersion = "5.0.14"
+val tokenValidationVersion = "5.0.5"
 val logstashVersion = "8.0"
-val springRetryVersion = "2.0.11"
-val springDocVersion = "2.8.1"
+val springRetryVersion = "2.0.10"
+val springDocVersion = "2.6.0"
 val springMockkVersion = "4.0.2"
 val logbackSyslog4jVersion = "1.0.0"
 val klageKodeverkVersion = "1.9.4"
-val testContainersVersion = "1.20.4"
-val otelVersion = "1.45.0"
+val testContainersVersion = "1.20.3"
+val otelVersion = "1.43.0"
 
 plugins {
-    val kotlinVersion = "2.1.0"
+    val kotlinVersion = "2.0.21"
     id("org.springframework.boot") version "3.3.5"
-    id("io.spring.dependency-management") version "1.1.7"
+    id("io.spring.dependency-management") version "1.1.6"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
