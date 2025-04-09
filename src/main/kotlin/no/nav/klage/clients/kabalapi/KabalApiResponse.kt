c3a63a234479ed8501c3eb82302a8d35851f7b6f
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2025-04-09 10:12:26 +0200

    Adjusted Kabal-Api response.

--- src/main/kotlin/no/nav/klage/clients/kabalapi/KabalApiResponse.kt
+++ src/main/kotlin/no/nav/klage/clients/kabalapi/KabalApiResponse.kt
@@ -303,8 +303,8 @@ data class DokumentReferanse(
     )
 
     data class AvsenderMottaker(
-        val id: String,
-        val type: AvsenderMottakerIdType,
+        val id: String?,
+        val type: AvsenderMottakerIdType?,
         val navn: String?,
     ) {
         enum class AvsenderMottakerIdType {
