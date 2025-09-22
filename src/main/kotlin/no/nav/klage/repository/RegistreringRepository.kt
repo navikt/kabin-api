0bfd5f0388273797dcc7e191f69656591be9967d
Andreas Jonsson <andreas.jonsson@nav.no>
2025-09-22 10:30:36 +0200

    Adding pessimistic locking when getting Registrering. Should fix issues we see with E2E-tests being very fast (or merkantil in theory).

--- src/main/kotlin/no/nav/klage/repository/RegistreringRepository.kt
+++ src/main/kotlin/no/nav/klage/repository/RegistreringRepository.kt
@@ -1,8 +1,12 @@
 package no.nav.klage.repository
 
+import jakarta.persistence.LockModeType
+import jakarta.persistence.QueryHint
 import no.nav.klage.domain.entities.Registrering
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import java.time.LocalDateTime
 import java.util.*
 
@@ -28,4 +32,10 @@ interface RegistreringRepository : JpaRepository<Registrering, UUID> {
     fun findFerdigeRegistreringer(navIdent: String, finishedFrom: LocalDateTime): List<Registrering>
 
     fun findByBehandlingId(behandlingId: UUID): Registrering
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(
+        QueryHint(name = "jakarta.persistence.lock.timeout", value = "20000"),
+    )
+    override fun findById(id: UUID): Optional<Registrering>
 }
