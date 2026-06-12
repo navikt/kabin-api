b92a27bfd056b483582095a46f10b9e02d4e2ced
Øyvind N. Wedøe <oyvind.norsted.wedoe@nav.no>
2026-06-12 09:53:44 +0200

    Spring Boot 4.0.7.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ val otelVersion = "1.62.0"
 
 plugins {
     val kotlinVersion = "2.3.21"
-    id("org.springframework.boot") version "4.0.6"
+    id("org.springframework.boot") version "4.0.7"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
@@ -46,16 +46,6 @@ dependencies {
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("ch.qos.logback:logback-classic")
-
-    //Fix vulnerabilities, while waiting for fix in Spring Boot.
-    implementation("org.apache.tomcat.embed:tomcat-embed-core:11.0.22")
-    implementation("io.netty:netty-codec-http:4.2.14.Final")
-    implementation("io.netty:netty-codec-http2:4.2.14.Final")
-    implementation("io.netty:netty-codec-http3:4.2.14.Final")
-    implementation("io.netty:netty-codec-dns:4.2.14.Final")
-    implementation("io.netty:netty-codec-compression:4.2.14.Final")
-    implementation("io.netty:netty-transport-native-epoll:4.2.14.Final")
-    implementation("io.netty:netty-resolver-dns:4.2.14.Final")
 
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("io.opentelemetry:opentelemetry-api:$otelVersion")
